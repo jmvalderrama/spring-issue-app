c752d334a6d59978d331a5f3157e7f402bbf7f0f
jmvalderrama <juanmv9402@gmail.com>
2023-03-09 13:44:29 -0500

    Default setup

---
+++ src/test/java/com/taller/issueapp/IssueappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller.issueapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
